ef2ef5e1f97c9bdec602eed662aa6dc0aab48a99
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-10-26 02:32:44 -0400

    just put the book model into typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,15 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
- 
+ type Book {
+  _id: ID
+  authors: [String]!
+  description: String
+  bookId: String
+  image: String
+  link: String
+  title: String
+  }
 `;
 
 module.exports = typeDefs;
